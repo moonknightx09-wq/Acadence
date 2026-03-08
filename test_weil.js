f88dbfd21941f13b58d51f51773479b39fb7d76a
Imran Farhat <moonknightx09@gmail.com>
2026-03-08 14:42:55 +0530

    update AI system and dashboard

--- test_weil.js
+++ test_weil.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import crypto from "crypto";
 
 const args = process.argv.slice(2);
@@ -17,24 +17,4 @@ const hash = crypto
     .update(JSON.stringify(record))
     .digest("hex");
 
-=======
-import crypto from "crypto";
-
-const args = process.argv.slice(2);
-
-const student = args[0];
-const score = args[1];
-
-const record = {
-    student: student,
-    score: score,
-    timestamp: Date.now()
-};
-
-const hash = crypto
-    .createHash("sha256")
-    .update(JSON.stringify(record))
-    .digest("hex");
-
->>>>>>> 4af0533a6ff38f39f444806c9ed2167a1f080f88
 console.log(hash);
